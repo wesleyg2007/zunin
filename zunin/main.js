eb61d42d4590c156effc4e04f13de1fb2e7427ce
wesleyg2007 <129761757+wesleyg2007@users.noreply.github.com>
2024-03-01 17:29:31 -0300

    Add files via upload

--- zunin/main.js
+++ zunin/main.js
@@ -1,16 +1,8 @@
-for (let i=0; i < botões.length; i++) {
-     console.log(i); 
-    }
-    for(let i=0;i <botoes.length;i++) {
-        botoes[i].onclick = function(){ 
-        botoes[i].classList.add(«ativo»);
-    }
-}
-const botoes = document.querySelectorAll(«.botao»);
+const botoes = document.querySelectorAll(".botao");
 for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
 
-for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove(«ativo»);
+for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove("ativo");
 }
-botoes[i].classList.add(«ativo»);
+botoes[i].classList.add("ativo");
 }
-}     
+}
